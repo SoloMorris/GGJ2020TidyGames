64ed528a3de9cd6e760604ce07df415931a25241
Woj <Wojciech2.Chmielewski@live.uwe.ac.uk>
2020-02-01 19:40:26 +0000

    Fixed controls problems again

--- GGJ2020TidyGames/Assets/Scripts/BarrelController.cs
+++ GGJ2020TidyGames/Assets/Scripts/BarrelController.cs
@@ -37,11 +37,11 @@ public class BarrelController : MonoBehaviour
     {
         if (aim.sqrMagnitude > 0.0f)
         {
-            transform.rotation = Quaternion.Euler(0, 0, transform.rotation.eulerAngles.z + 90);
-
-            targetRotation = Quaternion.LookRotation(aim, Vector3.back);
-            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, 4f * Time.deltaTime);
             transform.rotation = Quaternion.Euler(0, 0, transform.rotation.eulerAngles.z - 90);
+
+            targetRotation = Quaternion.LookRotation(aim, Vector3.forward);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, 4f * Time.deltaTime);
+            transform.rotation = Quaternion.Euler(0, 0, transform.rotation.eulerAngles.z + 90);
 
         }
        
