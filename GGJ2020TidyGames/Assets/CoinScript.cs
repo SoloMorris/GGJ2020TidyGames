071c76825b775d0f08013f4a3ac226fd9a323cfe
Woj <Wojciech2.Chmielewski@live.uwe.ac.uk>
2020-02-02 12:16:37 +0000

    Coins can be picked up and increase the score in game manager

---
+++ GGJ2020TidyGames/Assets/CoinScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour
+{
+    private GameManager gm;
+    // Start is called before the first frame update
+    void Start()
+    {
+       gm = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("TankRed"))
+        {
+            gm.scores.x += 1;
+            Destroy(this.gameObject);
+        }
+        else if (collision.gameObject.CompareTag("TankBlue"))
+        {
+            gm.scores.y += 1;
+            Destroy(this.gameObject);
+        }
+    }
+}
